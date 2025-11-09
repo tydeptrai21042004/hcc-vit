bf87a6d699d8acbf057692b1b21c6c2990b6bb5a
Dang Ba Ty <118130714+tydeptrai21042004@users.noreply.github.com>
2025-11-09 19:40:03 +0700

    Add files via upload

--- src/utils/io_utils.py
+++ src/utils/io_utils.py
@@ -31,29 +31,28 @@ class JSONEncoder(json.JSONEncoder):
             return int(obj)
         elif isinstance(obj, np.floating):
             return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
         else:
-            # return super(MyEncoder, self).default(obj)
-
             raise TypeError(
                 "Unserializable object {} of type {}".format(obj, type(obj))
             )
 
 
 def write_json(data: Union[list, dict], outfile: str) -> None:
+    """Write JSON with UTF-8 encoding."""
     json_dir, _ = os.path.split(outfile)
     if json_dir and not os.path.exists(json_dir):
         os.makedirs(json_dir)
 
-    with open(outfile, 'w') as f:
+    # Explicitly set encoding for safety
+    with open(outfile, 'w', encoding='utf-8') as f:
         json.dump(data, f, cls=JSONEncoder, ensure_ascii=False, indent=2)
 
 
 def read_json(filename: str) -> Union[list, dict]:
-    """read json files"""
-    with open(filename, "rb") as fin:
-        data = json.load(fin, encoding="utf-8")
+    """Read JSON file (Python 3.12 compatible)."""
+    # Open in text mode and DO NOT pass encoding to json.load
+    with open(filename, "r", encoding="utf-8") as fin:
+        data = json.load(fin)
     return data
 
 
